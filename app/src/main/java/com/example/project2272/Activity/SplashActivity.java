912220ff40a32fdc2ee8e0fbf72e51b128aec6c2
Huynhnmt <nghuynh5079@gmail.com>
2025-06-19 22:31:21 +0700

    47:08

--- app/src/main/java/com/example/project2272/Activity/SplashActivity.java
+++ app/src/main/java/com/example/project2272/Activity/SplashActivity.java
@@ -1,14 +1,10 @@
-package com.example.project2272;
+package com.example.project2272.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.project2272.databinding.ActivitySplashBinding;
 
---
+++ app/src/main/java/com/example/project2272/Domain/CategoryModel.java
@@ -0,0 +1,34 @@
+package com.example.project2272.Domain;
+
+public class CategoryModel {
+    private String Title;
+    private int id;
+    private String picUrl;
+
+    public CategoryModel() {
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+}
