6efca5bf2a2b0ba36c9ef981cdbb8265e99a9130
lethi <volethi30062003@gmail.com>
2025-06-19 22:51:43 +0700

    feature: category banner

---
+++ app/src/main/java/com/example/project2272/Activity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.project2272.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.project2272.Adapter.CategoryAdapter;
+import com.example.project2272.R;
+import com.example.project2272.ViewModel.MainViewModel;
+import com.example.project2272.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private MainViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        viewModel = new MainViewModel();
+
+        initCategory();
+
+    }
+
+    private void initCategory() {
+        binding.progressBarCategory.setVisibility(View.VISIBLE);
+        viewModel.loadCategory().observeForever(categoryModels -> {
+            binding.categoryView.setLayoutManager(new LinearLayoutManager(
+                    MainActivity.this, LinearLayoutManager.HORIZONTAL, false));
+            binding.categoryView.setAdapter(new CategoryAdapter(categoryModels));
+            binding.categoryView.setNestedScrollingEnabled(true);
+            binding.progressBarCategory.setVisibility(View.GONE);
+        });
+    }
+}
